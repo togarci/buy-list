d510c7fbb6c80d01178ebff91f91435dfa424da1
togarci <thyago.garcia10@gmail.com>
2025-08-11 02:00:38 -0300

    feat: create eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,44 @@
-// @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import vuePlugin from 'eslint-plugin-vue';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import prettierPlugin from 'eslint-plugin-prettier';
+import withNuxt from './.nuxt/eslint.config.mjs';
+import globals from 'globals';
 
-export default withNuxt(
-  // Your custom configs here
-)
+export default withNuxt([
+  {
+    files: ['**/*.{js,ts,vue}'],
+    languageOptions: {
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.es2021,
+      },
+    },
+    plugins: {
+      vue: vuePlugin,
+      '@typescript-eslint': tsPlugin,
+      prettier: prettierPlugin,
+    },
+    extends: [
+      'plugin:vue/vue3-recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:prettier/recommended',
+    ],
+    rules: {
+      'no-use-before-define': 'off',
+      '@typescript-eslint/no-use-before-define': ['error'],
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/ban-ts-comment': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { vars: 'all', args: 'none' },
+      ],
+      'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    },
+  },
+]);
